c3bd91b3dd06eb07050f5c8bebd4252aeba26911
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-03-05 12:25:12 -0500

    Initial commit
    base script and assets for testing

---
+++ README.md
@@ -0,0 +1,3 @@
+# stepmania js
+ 
+plays itg / stepmania 3.95 (or whatever itg is using) charts
---
+++ script.js
@@ -0,0 +1,55 @@
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+function step() {
+
+	window.setTimeout(step, 8.333333333333334) //120 tps
+	document.getElementById("tps").innerHTML = Math.round(1000 / 8.333333333333334)
+}
+
+var lastMs = 0
+function draw(ms) {
+	ctx.fillStyle = "magenta"
+	ctx.fillRect(0, 0, 640, 480)//temporary background
+
+	document.getElementById("fps").innerHTML = Math.round(1000 / (ms - lastMs))//fps counter
+	lastMs = ms
+
+	requestAnimationFrame(draw)
+}
+
+addEventListener("keydown", press(true))
+addEventListener("keyup", press(false))
+
+var keyInput = [
+	{
+		up: false,
+		down: false,
+		left: false,
+		right: false
+	},
+	{
+		up: false,
+		down: false,
+		left: false,
+		right: false
+	}
+]
+
+function press(v) {
+	return function (key) {
+		switch (key.code) {
+			case "ArrowUp":
+				keyInput[1].up = v; break
+			case "ArrowDown":
+				keyInput[1].down = v; break
+			case "ArrowLeft":
+				keyInput[1].left = v; break
+			case "ArrowRight":
+				keyInput[1].right = v; break
+		}
+	}
+}
+
+step()
+requestAnimationFrame(draw)
